64f47274644d8dbf98dcce316c3276930d7bd09f
Chih-Chen Kao <elros.morlin@gmail.com>
2017-11-21 15:03:03 +0800

    extract the common part

--- SimpleRT/kernel/RayTracing_Cm.cpp
+++ SimpleRT/kernel/RayTracing_Cm.cpp
@@ -8,27 +8,7 @@
 // #include "../include/Ray.hpp"
 
 
-#define FLOAT_PI 3.14159265358979323846f
-
-#define sign(x) ((x) > 0 ? 1 : -1)
-
-
-
-// simple impl.
-float clamp(float x, float low, float high) {
-	if (x < low) {
-		return low;
-	} else if (x > high) {
-		return high;
-	} else {
-		return x;
-	}
-}
-
-#define toInt(x) ((int)(cm_pow(clamp(x, 0.f, 1.f), 1.f / 2.2f) * 255.f + .5f))
-
-
-
+#include "RayTracing_Common.hpp"
 
 
 // we have to use C struct ...
@@ -42,28 +22,6 @@ float clamp(float x, float low, float high) {
 
 // tmp
 
-/* material types */
-enum Refl {
-	DIFF, SPEC, REFR
-};
-
-const float kEpsilon = 0.01f;
-
-
-const unsigned kWidth = 800;
-const unsigned kHeight = 600;
-
-constexpr const unsigned threadCountX = 100;
-constexpr const unsigned threadCountY = 100;
-
-
-constexpr const unsigned kSphereClassFloatcount =  12;       // padding
-constexpr const unsigned kCameraClassFloatcount =  15;       // check alignment
-
-constexpr const unsigned cameraVecSize = kCameraClassFloatcount;
-constexpr const unsigned rayVecSize = 6;
-
-constexpr const unsigned kColorFloatcount = 4;
 
 // not used, for reference only
 //struct Camera {
@@ -91,171 +49,11 @@ constexpr const unsigned kColorFloatcount = 4;
 
 
 // test 
-using CmRay = vector<float, rayVecSize>;
-using CmRay_ref = vector_ref<float, rayVecSize>;
-
-using CmCamera = vector<float, cameraVecSize>;
-using CmCamrea_ref = vector_ref<float, cameraVecSize>;
-
-using CmSeed_ref = vector_ref<unsigned, 2>;
-
-using CmSphere = vector<float, kSphereClassFloatcount>;
-using CmSphere_ref = vector_ref<float, kSphereClassFloatcount>;
-
-
-_GENX_ float vecDot(const vector_ref<float, 3> a, const vector_ref<float, 3> b) {
-    return a[0] * b[0] + a[1] * b[1] + a[2] * b[2];
-}
-
-_GENX_ bool vecIsZero(const vector_ref<float, 3> v) {
-    return (v[0] == 0) && (v[1] == 0) && (v[2] == 0);
-}
-
-_GENX_ vector<float, 3> vecCross(const vector_ref<float, 3> a, const vector_ref<float, 3> b) {
-    vector<float, 3> ret;
-
-    ret[0] = a[1] * b[2] - a[2] * b[1];
-    ret[1] = a[2] * b[0] - a[0] * b[2];
-    ret[2] = a[0] * b[1] - a[1] * b[0];
-
-    return ret;
-}
-
-_GENX_ void normalize(vector_ref<float, 3> v) {
-
-    // dot product
-    float dp = v[0] * v[0] + v[1] * v[1] + v[2] * v[2];
-    float f = 1.f / cm_sqrt(dp);        // Note: can change to rsqrt ...
-    v = f * v;
-}
-
-_GENX_ float getRandom(CmSeed_ref seeds) {
-
-    unsigned seed0 = seeds[0];
-    unsigned seed1 = seeds[1];
-
-    //printf("Cm before seed 0,1: %u, %u\n",seed0, seed1);
-
-    seeds[0] = 36969 * ((seed0) & 65535) + ((seed0) >> 16);
-	seeds[1] = 18000 * ((seed1) & 65535) + ((seed1) >> 16);
-    
-    unsigned int ires = ((seeds[0]) << 16) + (seeds[1]);
-
-    //printf("Cm seed 0,1: %u, %u  ires: %u\n",seed0, seed1, ires);
-    
-    /* Convert to float */
-    union {
-        float f;
-        unsigned int ui;
-    } res;
-    res.ui = (ires & 0x007fffff) | 0x40000000;
-
-    return (res.f - 2.f) / 2.f;
-}
 
 
 // Ray : Vec o, d => vector<float, 6>
 
 // for single ray ?
-_GENX_ void generateCameraRay(const CmCamrea_ref camera, vector_ref<unsigned, 2> seeds, const int width, const int height, const int x, const int y, CmRay_ref ray) {
-
-    //test
-    //seeds(0) = 10000;
-
-
-
-    const float invWidth = 1.f / width;
-	const float invHeight = 1.f / height;
-	const float r1 = getRandom(seeds) - 0.5f;
-	const float r2 = getRandom(seeds) - 0.5f;
-	const float kcx = (x + r1) * invWidth - 0.5f;
-    const float kcy = (y + r2) * invHeight - 0.5f;
-    
-
-//    if (x == 0 && y == 0) {
-//        printf("seed0, seed1 r1, r2: %u %u %f, %f | %f, %f: \n", seeds[0], seeds[1], r1, r2,  kcx, kcy);
-//    }
-
-    vector<float, 3> rdir;
-
-//    rdir(0) = camera(9) * kcx + camera(12) * kcy + camera(6);               // for x
-//    rdir(1) = camera(9 + 1) * kcx + camera(12 + 1) * kcy + camera(6 + 1);   // for y
-//    rdir(2) = camera(9 + 2) * kcx + camera(12 + 2) * kcy + camera(6 + 2);   // for z
-
-
-
-//    if (x == 0 && y == 0) {
-        //printf("camera x.x: %f \n", camera(9));
-//        printf("cam : %f, %f, %f, %f, %f, %f, %f, %f, %f\n", camera(6), camera(7), camera(8), camera(9), camera(10), camera(11) ,camera(12), camera(13), camera(14));
-
-
-//        printf("cam select: %f, %f\n", camera.select<3, 1>(9)[0], camera.select<3, 1>(9)[1]);
-
-//    }
-
-    rdir = (camera.select<3, 1>(9) * kcx) + (camera.select<3, 1>(12) * kcy) + camera.select<3, 1>(6);
-
-//   if (x == 0 && y == 0) {
-//		printf("rdir: x, y, z %f %f %f\n", rdir[0], rdir[1], rdir[2]);
-//	}
-
-    vector<float, 3> rorig = (0.1 * rdir) + camera.select<3, 1>(0);
-
-
-    // KAOCC:  rdir match with CL
-
-
-    // normalize rdir !!!
-
-    normalize(rdir);
-
-
-    ray.select<3, 1>(0) = rorig;
-    ray.select<3, 1>(3) = rdir;
-
-/*    for (auto i = 0 ; i < 3; ++i) {
-        ray[i] = rorig[i];
-        ray[i + 3] = rdir[i];
-    }*/
-
-//    if (x == 0 && y == 0) {
-//        printf("Ray Gen: %f ,%f, %f | %f, %f, %f\n", ray[0], ray[1], ray[2], ray[3], ray[4], ray[5]);
-//    }
-}
-
-
-
-_GENX_ float sphereIntersect(const CmSphere_ref sphere, const CmRay_ref ray) {
-
-
-    /* returns distance, 0 if nohit */
-    /* Solve t^2*d.d + 2*t*(o-p).d + (o-p).(o-p)-R^2 = 0 */
-
-    vector<float, 3> op = sphere.select<3, 1>(1) - ray.select<3, 1>(0);         // s->p - r->o
-    float b = vecDot(op, ray.select<3, 1>(3));     // op , r->d
-    float det = (b * b) - vecDot(op, op) + (sphere[0] * sphere[0]);
-
-    if (det < 0.f)
-        return 0.f;
-    else
-        det = cm_sqrt(det);
-
-
-
-    float t = b - det;
-    if (t >  kEpsilon)
-        return t;
-    else {
-        t = b + det;
-
-        if (t >  kEpsilon)
-            return t;
-        else
-            return 0.f;
-    }
-
-}
-
 
 
 // intersectResult: distance, sphere id
@@ -310,24 +108,6 @@ _GENX_ bool intersectP(SurfaceIndex spheresIndex, const unsigned kSphereCount, c
     return false;
 }
 
-_GENX_ vector<float, 3> uniformSampleSphere(const float u1, const float u2) {
-
-    const float z = 1.f - 2.f * u1;
-    const float r = cm_sqrt(cm_max<float>(0.f, 1.f - z * z));    //distance from the center
-
-    const float phi = 2.f * FLOAT_PI * u2;
-	const float x = r * cm_cos(phi);
-    const float y = r * cm_sin(phi);
-    
-
-    vector<float, 3> result;
-
-    result[0] = x;
-    result[1] = y;
-    result[2] = z;
-
-    return result;
-}
 
 _GENX_ void sampleLights(SurfaceIndex spheresIndex, const unsigned kSphereCount, CmSeed_ref seeds,  const vector_ref<float, 3> hitPoint, const vector_ref<float, 3> normal, vector_ref<float, 3> result) {
 
@@ -375,7 +155,6 @@ _GENX_ void sampleLights(SurfaceIndex spheresIndex, const unsigned kSphereCount,
             }
 
 
-
             /* Check if the light is visible */
 
             const float wi = vecDot(shadowRay.select<3, 1>(3), normal);
@@ -394,7 +173,6 @@ _GENX_ void sampleLights(SurfaceIndex spheresIndex, const unsigned kSphereCount,
     }
 
 }
-
 
 
 // single ray version
@@ -787,10 +565,6 @@ RayTracing(SurfaceIndex cameraIndex, SurfaceIndex seedIndex, SurfaceIndex colorI
             write(seedIndex,  seedOffset , seedIn);
 
 
-
-
-
-
             // pixel
             const unsigned pixelOffsetStart = (iNum / 4) * 4;
             const unsigned pixelLocalIndex = iNum % 4;
@@ -813,105 +587,5 @@ RayTracing(SurfaceIndex cameraIndex, SurfaceIndex seedIndex, SurfaceIndex colorI
     }
 
 
-
-
-/*
-    //  ------------ templates -------------
-
-    
-    unsigned firstSeedNumber = 2 * upperLeftPixelNumber;
-
-    // tmp
-    // Error: OWORD align ...
-
-    const unsigned offsetStart = (firstSeedNumber / 4) * 4;
-    const unsigned localIndex = firstSeedNumber % 4;
-
-    const unsigned seedOffset = offsetStart * 4;
-    
-    // KAOCC: Check offset !!!!!       It must be OWORD aligned !
-
-    vector<unsigned int, 4> seedIn;
-    read(seedIndex,  seedOffset , seedIn);
-
-    printf("firstSeedNumber: %u, Seed Offset: %u seedIn<4>: %u %u %u %u\n", firstSeedNumber, seedOffset, seedIn(0), seedIn(1), seedIn(2), seedIn(3));
-
-
-    // extra:
-
-    //vector<unsigned int, 4> seedIn;
-//    read(seedIndex,  16 , seedIn);
-
-//    printf("Extra: firstSeedNumber: %u, Seed Offset: %u seedIn<4>: %u %u %u %u\n", 4, 4 * 16, seedIn(0), seedIn(1), seedIn(2), seedIn(3));
-
-
-    vector<unsigned, 2> seedFinal = seedIn.select<2, 1>(localIndex);
-
-    CmRay ray;
-    generateCameraRay(camera, seedFinal, width, height, x,  y, ray);
-
-    vector<float, 3> result;
-    radiancePathTracing(spheresIndex, sphereCount, ray, seedFinal, result); 
-
-
-
-    // convert to pixel color
-
-
-    // ...
-
-
-    unsigned upperLeftColorNumber = upperLeftPixelNumber;
-    unsigned colorOffset = upperLeftColorNumber * 16;
-
-    // single color?
-    vector<float, kColorFloatcount> color;
-
-    // read color ?
-    read(colorIndex, colorOffset, color);
-
-    //printf("read color: %f %f %f %f\n", color[0], color[1], color[2], color[3]);
-    
-
-    // TMP
-    const unsigned currentSample = 0;       // Eventually it should be written from host
-    const float k1 = currentSample;
-    const float k2 = 1.f / (currentSample + 1.f);
-
-    color.select<3,1>(0) = (color.select<3,1>(0) * k1 + result) * k2;
-
-    // test only
-    color[3] = -1;
-
-
-    //printf("(%d, %d) write color: %f %f %f %f\n", x , y, color[0], color[1], color[2], color[3]);
-
-    write(colorIndex, colorOffset, color);
-
-
-*/
-
-//    if (x == 0 && y == 0) {
-//        printf("after function seedIn(0): %u\n", seedIn(0));
-//    }
-
-    //vector<unsigned int, 8> out;
-
-    //vector<Vec, 10> outColor;
-
-    //vector<float, 3> outTmpColor;
-
-    //printf("cam : %f, %f, %f, %f, %f, %f, %f, %f\n", vals(0), vals(1), vals(2), vals(3), vals(4), vals(5), vals(6), vals(7));
-
-    //printf("before out(0): %d\n", out(0));
-
-    //out(0) = 3000;
-    //out(1) = 5000;
-
-    //printf("out(0): %d\n", out(0));
-    //printf("out(1): %d\n", out(1));
-
-    //write(seedIndex, 0 , out);
-    //write(colorIndex, x, y, outColor);
-
 }
+
